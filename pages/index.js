df3cc983916d1f3610f966ab8cf0a338697f3abf
Felipe Broilo <felipebroilo@Mac-mini-de-Felipe.local>
2025-04-16 13:23:36 -0300

    primeiro deploy

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Bem-vindo ao sistema de plano de corte da 3F! 💡</h1>
+    </div>
+  )
+}
